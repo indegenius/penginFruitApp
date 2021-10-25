79f1e17641cd3d54e83ab8c8958182a831f6dd9b
indieurbanmusictv@gmail.com <indieurbanmusictv@gmail.com>
2021-10-25 16:01:47 -0400

    done for today

---
+++ models/connection.js
@@ -0,0 +1,27 @@
+/////////////////////////////////////
+// Import Our Dependencies
+/////////////////////////////////////
+require("dotenv").config() // Loading .env variables
+const mongoose = require("mongoose")
+
+//Connection
+// setup the inputs for mongoose connect
+const DATABASE_URL = process.env.DATABASE_URL // url from .env
+const CONFIG = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}
+
+// Connect to Mongo
+mongoose.connect(DATABASE_URL, CONFIG)
+
+//our connection messages
+mongoose.connection
+.on("open", () => console.log("Connected to Mongo"))
+.on("close", () => console.log("disconnected from mongo"))
+.on("error", (error) => console.log(error))
+
+//////////////////////////////
+// Export the Connection
+//////////////////////////////
+module.exports = mongoose
